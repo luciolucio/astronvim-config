94126bd747b0e77a0412c19d6868a2bf08978dc3
Lucio Assis <luciolucio@gmail.com>
2023-10-23 18:33:08 -0300

    Increase notification timeout

--- plugins/core.lua
+++ plugins/core.lua
@@ -89,7 +89,7 @@ return {
     "rcarriga/nvim-notify",
     opts = {
       top_down = false,
-      timeout = 1000,
+      timeout = 3000,
       -- log level - 3 hide file write messages - default 5
       level = 3,
       -- background_color = "#000000",
